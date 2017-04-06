b8a9eb12772ed99c84084810a87a84f81908e1ab
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-06 22:13:52 +0800

    prototypes design

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by John Crisostomo on 06/04/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
