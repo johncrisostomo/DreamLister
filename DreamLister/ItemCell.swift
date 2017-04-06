13d63a31f6f297d04344aebddde27fa916a223b0
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-06 22:35:25 +0800

    prepares views for core data integration

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by John Crisostomo on 06/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+}
